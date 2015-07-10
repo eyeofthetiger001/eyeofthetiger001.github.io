d078f4158e29db377ee3782ea6a68b6cf5978662
Mahnoor Malik <eyeofthetiger001@users.noreply.github.com>
2015-07-10 12:35:22 -0400

    prototype

--- effects.js
+++ effects.js
@@ -1,6 +1,44 @@
 confirm("Enter moo land?");
 $(document).ready(function(){
+  $("img").hide();
   $("#button").click(function(){
+    $("#screen").css("background-color","#f1e1da");
+    $("#screen").css("border","solid #f1e1da");
     $("#button").fadeOut("fast");
+
+  var numImages = $(".deck img").length;
+
+  var count = 0;
+  $img = document.getElementById("first");
+  var timerId;
+  recurImage();
+
+  function recurImage(){
+    if (count == numImages){
+      clearTimeout(timerId);
+      return;
+    }
+    else if (count == 0){
+      /*$($img).css("display","block");*/
+      $($img).fadeIn("slow");
+      count++;
+      timerId = setTimeout(recurImage,3000);
+
+    }
+    else{
+      $($img).fadeOut("slow");
+      $img = $($img).next();
+      $($img).fadeIn("slow");
+      count++;
+      timerId = setTimeout(recurImage,3000);
+    }
+
+  }
+
+
+
 });
+
+
+
 });
